9ef877e7f1e0f4685223994cef59ecaf49c0379f
Hoa Dam <hoangocdam@gmail.com>
2024-01-09 11:01:31 -0800

    test:iteration 4

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -85,4 +85,106 @@ RSpec.describe Carnival do
       expect(carnival.total_revenue).to eq(9)
     end
   end
+
+  describe '#summary' do
+    it 'provides a summary hash' do
+      carnival.add_ride(ride1)
+      carnival.add_ride(ride2)
+      carnival.add_ride(ride3)
+
+      visitor1.add_preferences(:gentle)
+      visitor2.add_preferences(:gentle)
+      visitor3.add_preferences(:gentle)
+      visitor3.add_preferences(:thrilling)
+      visitor2.add_preferences(:thrilling)
+
+      ride1.board_rider(visitor1)
+      ride1.board_rider(visitor1)
+      ride2.board_rider(visitor2)
+      ride2.board_rider(visitor3)
+      ride3.board_rider(visitor3)
+
+      expect(carnival.summary).to eq({
+        visitor_count: 3,
+        revenue_earned: 14,
+        visitors: [
+          {
+            visitor: visitor1,
+            favorite_ride: ride1,
+            total_money_spent: 2
+          },
+          {
+            visitor: visitor2,
+            favorite_ride: ride2,
+            total_money_spent: 5
+          },
+          {
+            visitor: visitor3,
+            favorite_ride: ride2,
+            total_money_spent: 7
+          }
+        ],
+        rides: [
+          {
+            ride: ride1,
+            riders: [visitor1],
+            total_revenue: 2
+          },
+          {
+            ride: ride2,
+            riders: [visitor2, visitor3],
+            total_revenue: 10
+          },
+          {
+            ride: ride3,
+            riders: [visitor3],
+            total_revenue: 2
+          }
+        ]
+      })
+    end
+  end
+
+  describe '#visitor_count' do
+    it 'counts the total visitors in each carnival' do
+      carnival.add_ride(ride1)
+      carnival.add_ride(ride2)
+      carnival.add_ride(ride3)
+
+      visitor1.add_preferences(:gentle)
+      visitor2.add_preferences(:gentle)
+      visitor3.add_preferences(:thrilling)
+      visitor2.add_preferences(:thrilling)
+
+      ride1.board_rider(visitor1)
+      ride1.board_rider(visitor1)
+      ride2.board_rider(visitor2)
+      ride2.board_rider(visitor3)
+      ride3.board_rider(visitor3)
+
+      expect(carnival.visitor_count).to eq(3)
+    end
+  end
+
+  describe '#revenue_earned' do
+    it 'returns the total revenue earned' do
+      carnival.add_ride(ride1)
+      carnival.add_ride(ride2)
+      carnival.add_ride(ride3)
+
+      visitor1.add_preferences(:gentle)
+      visitor2.add_preferences(:gentle)
+      visitor3.add_preferences(:thrilling)
+      visitor2.add_preferences(:thrilling)
+
+      ride1.board_rider(visitor1)
+      ride1.board_rider(visitor1)
+      ride2.board_rider(visitor2)
+      ride2.board_rider(visitor3)
+      ride3.board_rider(visitor3)
+
+      expect(carnival.revenue_earned).to eq(14)
+    end
+
+  end
 end
